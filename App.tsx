da6144359b749f76276fe919b9e9957df20ef311
Mateus Leonardo <m4teusmatos@gmail.com>
2022-12-17 22:29:22 -0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { FlatList, ScrollView, StyleSheet, Text, View } from 'react-native';
+
+function Conversations() {
+  return <Text>Conversations</Text>
+}
+
+function Calls() {
+  return <Text>Calls</Text>
+}
+
+function Status() {
+  return <Text>Status</Text>
+}
+
+interface User {
+  id: number;
+  name: string;
+  message: string;
+  photo: any;
+}
 
 export default function App() {
+
+  const [users, setUsers] = useState<User[]>([
+    {
+      id: 1,
+      name: 'George',
+      message: 'Hello',
+      photo: 1
+    },
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <ScrollView>
+        <FlatList
+          data={users}
+          keyExtractor={(item) => String(item?.id)}
+          renderItem={({ item }) => <Conversations />}
+        />
+      </ScrollView>
     </View>
   );
 }
